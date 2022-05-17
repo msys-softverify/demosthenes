6bf24d2adc6e579c5afa00ce3f4a1f81143120c2
Junko Ishii <ishii-junko@marubeni-sys.com>
2022-05-17 20:59:53 +0900

    fix codes

--- revisions/rev4/driver/entry.c
+++ revisions/rev4/driver/entry.c
@@ -1,14 +1,3 @@
 int main() {
-    int a[] = {1, 2, 3};
-    int b;
-    int i;
-    for(i=0; i < sizeof(a); i++) {
-        b += a[i];
-    }
-    int c[] = {1, 2, 3};
-    int d;
-    for(i=0; i < sizeof(a); i++) {
-        d += c[i];
-    }
     return 0;
 }
