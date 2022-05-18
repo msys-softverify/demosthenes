317bc13e8d654f73a6e85546b9f7cf35957b579e
Junko Ishii <ishii-junko@marubeni-sys.com>
2022-05-18 12:06:27 +0900

    First commit for featureB

--- revisions/rev4/driver/entry.c
+++ revisions/rev4/driver/entry.c
@@ -1,4 +1,10 @@
 int main() {
-    int a;
-    return 0;
+    int a[] = {1,2,3,4,5};
+    int ret;
+    int i;
+    for(i=0; i<sizeof(a); i++)
+    {
+        ret = ret + a[i];
+    }
+    return ret;
 }
