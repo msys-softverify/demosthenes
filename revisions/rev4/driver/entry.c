feaa44502444b50a63fe6f1f30ab7bcb9067619b
Junko Ishii <ishii-junko@marubeni-sys.com>
2022-05-18 12:15:58 +0900

    fix issues

--- revisions/rev4/driver/entry.c
+++ revisions/rev4/driver/entry.c
@@ -1,8 +1,8 @@
 int main() {
     int a[] = {1,2,3,4,5};
-    int ret;
+    int ret = 0;
     int i;
-    for(i=0; i<sizeof(a); i++)
+    for(i=0; i<sizeof(a)/sizeof(int); i++)
     {
         ret = ret + a[i];
     }
