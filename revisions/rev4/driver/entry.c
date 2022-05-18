ce046c7b0f4ea9290f5bf26719832b38e7200316
msysji <Ishii-Junko@marubeni-sys.com>
2022-05-18 12:58:27 +0900

    featureB (#4)
    * First commit for featureB
    
    * fix issues
    
    * modified for featureB
    
    * revert the change

--- revisions/rev4/driver/entry.c
+++ revisions/rev4/driver/entry.c
@@ -1,4 +1,10 @@
 int main() {
-    int a;
-    return 0;
+    int a[] = {1,2,3,4,5};
+    int ret = 0;
+    int i;
+    for(i=0; i<sizeof(a)/sizeof(int); i++)
+    {
+        ret = ret + a[i];
+    }
+    return ret;
 }
