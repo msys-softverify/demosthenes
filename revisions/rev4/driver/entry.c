932dd59f27e2a3e0574750e0d8ae8136c2f1031b
msysji <Ishii-Junko@marubeni-sys.com>
2023-04-25 15:42:31 +0900

    Update entry.c

--- revisions/rev4/driver/entry.c
+++ revisions/rev4/driver/entry.c
@@ -1,16 +1,21 @@
-int foo() {
-    int a;
-    int b;
-    return a + b;
+#import <stdlib.h>
+
+int *foo(int n) {
+    int *ptr = malloc(sizeof(int));
+    if(!ptr) {
+        return NULL;
+    }
+    *ptr = n;
+    
+    return ptr;
+}
+
+int bar() {
+    int *p = foo(10);
+    if (p != NULL) {}
+    p[0] = 0;
 }
 
 int main() {
-    int a[] = {1,2,3,4,5};
-    int ret = 0;
-    int i;
-    for(i=0; i<sizeof(a)/sizeof(int); i++)
-    {
-        ret = ret + a[i];
-    }
-    return ret;
+    return 0;
 }
