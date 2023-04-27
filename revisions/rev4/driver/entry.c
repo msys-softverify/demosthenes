56ceb9a3a7e2a8e17682265e70de2b1526cf4559
msysji <Ishii-Junko@marubeni-sys.com>
2023-04-27 12:28:42 +0900

    update file

--- revisions/rev4/driver/entry.c
+++ revisions/rev4/driver/entry.c
@@ -3,26 +3,6 @@ int foo() {
     int b;
     return a + b;
 }
-int bar() {
-    int c[] = {1, 2, 3};
-    int i;
-    int ret;
-    for(i<0; i<sizeof(c); i++)
-    {
-        ret = ret + c[i];
-    }
-    return ret;
-}
-int hoge() {
-    int d[] = {1, 2, 3};
-    int i;
-    int ret;
-    for(i<0; i<sizeof(d); i++)
-    {
-        ret = ret + d[i];
-    }
-    return ret;
-}
 
 int main() {
     int a[] = {1,2,3,4,5};
