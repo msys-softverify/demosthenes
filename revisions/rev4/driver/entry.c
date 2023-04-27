88227bab2d31ba7925cfa126ba0dacab779e3cf1
msysji <Ishii-Junko@marubeni-sys.com>
2023-04-27 12:32:31 +0900

    last test

--- revisions/rev4/driver/entry.c
+++ revisions/rev4/driver/entry.c
@@ -1,7 +1,12 @@
 int foo() {
-    int a;
-    int b;
-    return a + b;
+    int a[] = {1,2,3,4,5};
+    int ret = 0;
+    int i;
+    for(i=0; i<sizeof(a)/sizeof(int); i++)
+    {
+        ret = ret + a[i];
+    }
+    return ret;
 }
 
 int main() {
